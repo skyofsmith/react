3465170b1e86707df0a5e7b42a24d9b564e6822b
drzz <skyofsmith@163.com>
2018-10-26 00:36:40 +0800

    useJSX

--- runoob/useJSX.js
+++ runoob/useJSX.js
@@ -1,4 +1,16 @@
+var myStyle = {
+    fontSize: 100,
+    color: '#FF0000'
+};
+var arr = [
+    <h1>菜鸟教程</h1>,
+    <h2>学的不仅是技术，更是梦想！</h2>,
+];
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+    <div>
+        {/*注释...*/}
+        {arr}
+        <h1 style={myStyle}>Hello, world! {1+1}</h1>
+    </div>,
   document.getElementById('example')
 );
