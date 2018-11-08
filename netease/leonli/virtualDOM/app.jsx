9001f4acb93d894f6039a7593e88911b424f40df
drzz <skyofsmith@163.com>
2018-11-08 23:50:29 +0800

    update data when click h1

--- netease/leonli/virtualDOM/app.jsx
+++ netease/leonli/virtualDOM/app.jsx
@@ -1,13 +1,21 @@
 class Count extends React.Component {
     constructor(){
+        super();
         this.state = {
             count: 0
         };
     }
+
+    update(){
+        this.setState({
+            count: ++this.state.count
+        });
+    }
+
     render(){
         return (
             <div>
-                <h1>Count</h1>
+                <h1 onClick={this.update.bind(this)}>Count</h1>
                 <h3>{this.state.count}</h3>
             </div>
         );
