2face6771c2e43d85e8c7f046fc1c92a55ce3742
skyofsmith <skyofsmith@163.com>
2020-06-08 22:50:04 +0800

    combine extends

--- doc_learn/my-app/src/App.js
+++ doc_learn/my-app/src/App.js
@@ -131,6 +131,79 @@ class Calculator extends React.Component {
   }
 }
 
+function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
+    </div>
+  );
+}
+
+function WelcomeDialog() {
+  return (
+    <Dialog
+      title="Welcome"
+      message="Thank you for visiting our spacecraft!" />
+  );
+}
+
+function SplitPane(props) {
+  return (
+    <div className="SplitPane">
+      <div className="SplitPane-left">
+        {props.left}
+      </div>
+      <div className="SplitPane-right">
+        {props.right}
+      </div>
+    </div>
+  );
+}
+
+function Dialog(props) {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        {props.title}
+      </h1>
+      <p className="Dialog-message">
+        {props.message}
+      </p>
+    </FancyBorder>
+  );
+}
+
+
+class SignUpDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = {login: ''};
+  }
+
+  render() {
+    return (
+      <Dialog title="Mars Exploration Program"
+              message="How should we refer to you?">
+        <input value={this.state.login}
+               onChange={this.handleChange} />
+        <button onClick={this.handleSignUp}>
+          Sign Me Up!
+        </button>
+      </Dialog>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({login: e.target.value});
+  }
+
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}!`);
+  }
+}
+
 function App() {
   const welcomeComp = <Welcome name='tom'></Welcome>
   return (
@@ -142,6 +215,17 @@ function App() {
       <Clock />
       <Clock />
       <Calculator />
+      <hr/>
+      <WelcomeDialog />
+      <SplitPane
+        left={
+          <div>left</div>
+        }
+        right={
+          <div>left</div>
+        }
+      />
+      <SignUpDialog />
     </div>
   );
 }
