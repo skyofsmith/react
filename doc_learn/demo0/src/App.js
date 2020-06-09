2830108e6e7f652c9e94798e3e6f0ae8b824e4af
skyofsmith <skyofsmith@163.com>
2020-06-09 22:09:15 +0800

    hook

--- doc_learn/demo0/src/App.js
+++ doc_learn/demo0/src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+function Example() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
+  })
+  return (
+    <div>
+      <p>You clicked <span style={{'font-size': '50px'}}>{count}</span> times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  )
+}
+
+function ExampleWithManyStates() {
+  // 声明多个 state 变量！
+  const [age, setAge] = useState(42);
+  const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState([{ text: 'Learn Hooks' }]);
+  return (
+    <div>
+      <p key={1}>
+        <span>age is {age}</span>
+        <button onClick={() => setAge(age+1)}>+</button>
+      </p>
+      <p key={2}>
+        <span>fruit is {fruit}</span>
+        <button onClick={() => setFruit(fruit+'1')}>+</button>
+      </p>
+      <p key={3}>
+        <span>todos is {todos.map(s => s.text).join(',')}</span>
+        <button onClick={() => setTodos(todos.concat({text: 'Learn Hooks2'}))}>+</button>
+      </p>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Example />
+      <ExampleWithManyStates />
     </div>
   );
 }
